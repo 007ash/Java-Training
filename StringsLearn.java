c614a6c0a358a39874f48850ab198a32fbe9098f
007ash <6ashwanth007@gmail.com>
2025-09-02 18:01:59 +0530

    str

--- StringsLearn.java
+++ StringsLearn.java
@@ -1,7 +1,9 @@
 import java.util.*;
 public class StringsLearn{
     public static void main(String[] args){
+
         String sc = "Hello World";
         System.out.println(sc.length());
+        
     }
 }
