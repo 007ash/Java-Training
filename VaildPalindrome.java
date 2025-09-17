3066c971b8b5d8c9f951715dd6b4c01a7b3aca03
007ash <6ashwanth007@gmail.com>
2025-09-17 22:54:23 +0530

    valid palindrome

--- VaildPalindrome.java
+++ VaildPalindrome.java
@@ -1,6 +1,5 @@
 public class VaildPalindrome {
-
-    public static void isPalindrome(String s) {
+    public static boolean isPalindrome(String s) {
         s = s.toLowerCase();
         String str = "";
         int n = s.length();
@@ -12,13 +11,20 @@ public class VaildPalindrome {
                 str += ch;
             }
         }
-        System.out.println(str);
-
+        boolean flag = true;
+        for(int i=0; i<str.length(); i++){
+            char ch1 = str.charAt(i);
+            char ch2 = str.charAt(str.length()-1-i);
+            if(!(ch1 == ch2)){
+                flag = false;
+            }
+        }
+        return flag;
     }
 
     public static void main(String[] args){
         String str = "A man, a plan, a canal: Panama";
-        isPalindrome(str);
+        System.out.println(isPalindrome(str));
 
     }
 }
